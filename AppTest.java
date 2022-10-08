c3f82699fbb70e67debc5187f9843462a811177c
vickyiny <108565138+vickyiny@users.noreply.github.com>
2022-10-08 10:46:08 +0530

    Add files via upload

---
+++ AppTest.java
@@ -0,0 +1,13 @@
+package com.playwright.playwright;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class AppTest {
+    
+  @Test
+  public void shouldAnswerWithTrue() {
+    assertTrue(true);
+  }
+}
